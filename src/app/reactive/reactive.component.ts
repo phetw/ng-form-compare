851c67942ef241b6ef5ac9e22016ef43a89b0743
Wasuwat Limsuparhat <wasuwat.se@gmail.com>
2019-02-28 14:50:49 +0700

    add docs

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ *
+ * Reactive form
+ *
+ * more robust: they're more scalable, reusable, and testable.
+ * If forms are a key part of your application,
+ * or you're already using reactive patterns for building your application, use reactive forms.
+ *
+ */
+
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ *
+ * Template-driven form
+ *
+ * useful for adding a simple form to an app, such as an email list signup form.
+ * They're easy to add to an app, but they don't scale as well as reactive forms.
+ * If you have very basic form requirements and logic that can be managed solely in the template, use template-driven forms.
+ *
+ *
+ */
+
 @Component({
   selector: 'app-template-driven',
   templateUrl: './template-driven.component.html',
